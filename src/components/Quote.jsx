a65c023da0d3c68b86bc62875edc27452974f4ea
BiancaMesa <bianca.koraa@gmail.com>
2024-05-16 21:10:30 +0200

    quote section video added

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -3,7 +3,11 @@ import "../scss/components/Quote.scss";
 function Quote() {
   return (
     <section className="quoteSection">
-        <img className="quoteSection__image" src="/public/yoga_sunset.jpg" alt="yoga_sunset" />
+        {/* <img className="quoteSection__image" src="/public/yoga_sunset.jpg" alt="yoga_sunset" /> */}
+
+        <video className="quoteSection__video" autoplay loop >
+            <source src="/public/video1.mp4" type="video/mp4"/>
+        </video>
 
         <h2 className="quoteSection__quote">Take care of your purpose and your purpose will take care of you</h2>
     </section>
