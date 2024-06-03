624bed10a2ba5915b9f133405e5ac75dacbf27b0
BiancaMesa <bianca.koraa@gmail.com>
2024-06-03 20:58:59 +0200

    carousel styles added

--- src/components/CarouselProjects.jsx
+++ src/components/CarouselProjects.jsx
@@ -1,8 +1,8 @@
 import Carousel from 'react-bootstrap/Carousel';
 import rickAndMorty from "../images/rick2.png";
-import catCharity from "../images/cat_charity.png";
-//import "../scss/components/Projects.scss";
+import catCharity from "../images/cat_charity2.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../scss/components/CarouselProjects.scss";
 
 function CarouselProjects () {
 
@@ -11,62 +11,40 @@ function CarouselProjects () {
       "image": rickAndMorty,
       "title": "Rick and Morty Character Finder",
       "description": "Project Description",
-      "technologies": "tech used icons"
+      "technologies": "tech used icons",
+      "alt": "First slide"
     },
     {
       "image": catCharity,
       "title": "Gatos de Ogíjares",
       "description": "Project Description",
-      "technologies": "tech used icons"
+      "technologies": "tech used icons",
+      "alt": "Second slide"
     }
-  ]
+  ];
+
+  // function ControlledCarousel() {
+  //   const [index, setIndex] = useState(0);
+  
+  //   const handleSelect = (selectedIndex) => {
+  //     setIndex(selectedIndex);
+  //   };
 
   return (
-    <Carousel fade>
+    <Carousel slide={false}>
 
       {projectSlides.map((projectSlide, index) => (
-        <Carousel.Item className="projects__project" key={index}>
-        <Carousel.Caption>
-          <h3 className="projectTitle">{projectSlide.title}</h3>
-          <p className="projectDescription">{projectSlide.description}</p>
-          <span>{projectSlide.technologies}</span>
+        <Carousel.Item className="project"  key={index}>
+          <Carousel.Caption className="project__info">
+            <h3 className="project__info--title">{projectSlide.title}</h3>
+            <p className="project__info--description">{projectSlide.description}</p>
+            <span className="project__info--technologies">{projectSlide.technologies}</span>
           </Carousel.Caption>
-        <img src={projectSlide.image} className="projects__project--image" alt={projectSlide.title}  />
+          
+         <img src={projectSlide.image} className="project__image" alt={projectSlide.alt}  />
         </Carousel.Item>
       ))}
-  </Carousel>
-
-
-
-    // <Carousel fade>
-
-    //   <Carousel.Item className="projects__project">
-    //     <Carousel.Caption>
-    //       <h3 className="projectTitle">Rick and Morty Character Finder</h3>
-    //       <p className="projectDescription">Project description ..kj ajskkdajbs </p>
-    //       <span>TECH used icons</span>
-    //       </Carousel.Caption>
-    //     <img src={rickAndMorty} className="projects__project--image" alt="Rick and Morty"  />
-    //   </Carousel.Item>
-
-    //   <Carousel.Item className="projects__project">
-    //     <Carousel.Caption>
-    //       <h3 className="projectTitle">Gatos de Ogíjares</h3>
-    //               <p className="projectDescription">Project description ..kj ajskkdajbs </p>
-    //               <span>TECH used icons</span>
-    //       </Carousel.Caption>
-    //     <img src={catCharity} className="projects__project--image" alt="Cat Charity" />
-    //   </Carousel.Item>
-
-    //   <Carousel.Item className="projects__project">
-    //     <Carousel.Caption>
-    //       <h3 className="projectTitle">Gatos de Ogíjares</h3>
-    //               <p className="projectDescription">Project description ..kj ajskkdajbs </p>
-    //               <span>TECH used icons</span>
-    //       </Carousel.Caption>
-    //     <img src={catCharity} className="projects__project--image" alt="Cat Charity" />
-    //   </Carousel.Item>
-    // </Carousel>
+    </Carousel>
   );
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,4 @@
 import "../scss/components/Projects.scss";
-import rick from "../images/rick2.png";
-//import catCharity from "../images/cat_charity.png";
-//import { Carousel } from "react-bootstrap";
 import CarouselProjects from './CarouselProjects';
 
 function Projects() {
@@ -11,38 +8,6 @@ function Projects() {
 
         <CarouselProjects />
 
-
-
-
-        {/* <div className="projects__project">
-            {/* <i className="fa-solid fa-angle-left projects__project--angle"></i> */}
-{/* 
-            <div className="projects__project--info">
-                <h3 className="projectTitle">Rick and Morty Character Finder</h3>
-                <p className="projectDescription">Project description ..kj ajskkdajbs </p>
-                <span>TECH used icons</span>
-            </div>
-            
-            <img className="projects__project--image" src={rick} alt="rick" />
-
-            {/* <i className="fa-solid fa-angle-right projects__project--angle"></i> */}
-        {/* </div> */} 
-
-
-
-        {/* <div className="projects__project">
-            <i className="fa-solid fa-angle-left projects__project--angle"></i>
-            
-            <div className="projects__project--info">
-                <h3 className="projectTitle">Gatos de Ogíjares</h3>
-                <p className="projectDescription">Project description ..kj ajskkdajbs </p>
-                <span>TECH used icons</span>
-            </div>
-            
-            <img className="projects__project--image" src={catCharity} alt="rick" />
-
-            <i className="fa-solid fa-angle-right projects__project--angle"></i>
-        </div> */}
     </section>
   )
 }
