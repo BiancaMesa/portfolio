34c1c4b1d2e258979d327da326095767b755af25
BiancaMesa <bianca.koraa@gmail.com>
2024-06-03 19:49:44 +0200

    map for carousel

--- src/components/CarouselProjects.jsx
+++ src/components/CarouselProjects.jsx
@@ -5,36 +5,68 @@ import catCharity from "../images/cat_charity.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CarouselProjects () {
+
+  const projectSlides = [
+    {
+      "image": rickAndMorty,
+      "title": "Rick and Morty Character Finder",
+      "description": "Project Description",
+      "technologies": "tech used icons"
+    },
+    {
+      "image": catCharity,
+      "title": "Gatos de Ogíjares",
+      "description": "Project Description",
+      "technologies": "tech used icons"
+    }
+  ]
+
   return (
     <Carousel fade>
 
-      <Carousel.Item className="projects__project">
+      {projectSlides.map((projectSlide, index) => (
+        <Carousel.Item className="projects__project" key={index}>
         <Carousel.Caption>
-          <h3 className="projectTitle">Rick and Morty Character Finder</h3>
-                  <p className="projectDescription">Project description ..kj ajskkdajbs </p>
-                  <span>TECH used icons</span>
+          <h3 className="projectTitle">{projectSlide.title}</h3>
+          <p className="projectDescription">{projectSlide.description}</p>
+          <span>{projectSlide.technologies}</span>
           </Carousel.Caption>
-        <img src={rickAndMorty} className="projects__project--image" alt="Rick and Morty"  />
-      </Carousel.Item>
+        <img src={projectSlide.image} className="projects__project--image" alt={projectSlide.title}  />
+        </Carousel.Item>
+      ))}
+  </Carousel>
 
-      <Carousel.Item className="projects__project">
-        <Carousel.Caption>
-          <h3 className="projectTitle">Gatos de Ogíjares</h3>
-                  <p className="projectDescription">Project description ..kj ajskkdajbs </p>
-                  <span>TECH used icons</span>
-          </Carousel.Caption>
-        <img src={catCharity} className="projects__project--image" alt="Cat Charity" />
-      </Carousel.Item>
 
-      <Carousel.Item className="projects__project">
-        <Carousel.Caption>
-          <h3 className="projectTitle">Gatos de Ogíjares</h3>
-                  <p className="projectDescription">Project description ..kj ajskkdajbs </p>
-                  <span>TECH used icons</span>
-          </Carousel.Caption>
-        <img src={catCharity} className="projects__project--image" alt="Cat Charity" />
-      </Carousel.Item>
-    </Carousel>
+
+    // <Carousel fade>
+
+    //   <Carousel.Item className="projects__project">
+    //     <Carousel.Caption>
+    //       <h3 className="projectTitle">Rick and Morty Character Finder</h3>
+    //       <p className="projectDescription">Project description ..kj ajskkdajbs </p>
+    //       <span>TECH used icons</span>
+    //       </Carousel.Caption>
+    //     <img src={rickAndMorty} className="projects__project--image" alt="Rick and Morty"  />
+    //   </Carousel.Item>
+
+    //   <Carousel.Item className="projects__project">
+    //     <Carousel.Caption>
+    //       <h3 className="projectTitle">Gatos de Ogíjares</h3>
+    //               <p className="projectDescription">Project description ..kj ajskkdajbs </p>
+    //               <span>TECH used icons</span>
+    //       </Carousel.Caption>
+    //     <img src={catCharity} className="projects__project--image" alt="Cat Charity" />
+    //   </Carousel.Item>
+
+    //   <Carousel.Item className="projects__project">
+    //     <Carousel.Caption>
+    //       <h3 className="projectTitle">Gatos de Ogíjares</h3>
+    //               <p className="projectDescription">Project description ..kj ajskkdajbs </p>
+    //               <span>TECH used icons</span>
+    //       </Carousel.Caption>
+    //     <img src={catCharity} className="projects__project--image" alt="Cat Charity" />
+    //   </Carousel.Item>
+    // </Carousel>
   );
 }
 
