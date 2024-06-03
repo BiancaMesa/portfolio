7c13259c408e514042fcb5ff06dcec3b95fce341
BiancaMesa <bianca.koraa@gmail.com>
2024-06-03 21:30:23 +0200

    coaching and yoga added to video component

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -10,7 +10,16 @@ function Quote() {
             <source src={quoteVideo} type="video/mp4"/>
         </video>
 
-        <h2 className="quoteSection__quote">Take care of your purpose and your purpose will take care of you</h2>
+        {/* <h2 className="quoteSection__quote">Take care of your purpose and your purpose will take care of you</h2> */}
+        <div className="quoteSection__coaching">
+          <h2 className="quoteSection__coaching--title">Coaching</h2>
+          <p className="quoteSection__coaching--description">Lorem...</p>
+        </div>
+
+        <div className="quoteSection__yoga">
+          <h2 className="quoteSection__yoga--title">Yoga</h2>
+          <p className="quoteSection__yoga--description">Lorem...</p>
+        </div>
     </section>
   )
 }
