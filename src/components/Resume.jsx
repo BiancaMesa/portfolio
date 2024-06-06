29a3c698ac977edfe7516b0bf279f33e610ee855
BiancaMesa <bianca.koraa@gmail.com>
2024-06-06 18:23:25 +0200

    techs and skills size fixed and techs wrap added

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -20,23 +20,19 @@ function Resume() {
                 <h2 className="resume__techs--title">Technologies and tools</h2>
                 {/* <h3>Love them or hate them?</h3> */}
                 <div className="resume__techs--techs">
-                    {/* <i className="fa-brands fa-js techIcon"></i>
-                    <i className="fa-brands fa-react techIcon"></i>
-                    <i className="fa-brands fa-html5 techIcon"></i>
-                    <i className="fa-brands fa-css3-alt techIcon"></i>
-                    <i className="fa-brands fa-sass techIcon"></i>
-                    <i className="fa-brands fa-git techIcon"></i>
-                    <i className="fa-brands fa-github techIcon"></i>
-                    <i className="fa-brands fa-node techIcon"></i> */}
-                    <img className="techIconImage" src={javaScriptIcon} alt="javascript_icon" />
-                    <img className="techIconImage" src={reactIcon} alt="react_icon" />
-                    <img className="techIconImage" src={htmlIcon} alt="html_icon" />
-                    <img className="techIconImage" src={cssIcon} alt="css_icon" />
-                    <img className="techIconImage" src={sassIcon} alt="sass_icon" />
-                    <img className="techIconImage" src={bootstrap} alt="bootstrap_icon" />
+                    {/* <div> */}
+                        <img className="techIconImage" src={javaScriptIcon} alt="javascript_icon" />
+                        <img className="techIconImage" src={reactIcon} alt="react_icon" />
+                        <img className="techIconImage" src={htmlIcon} alt="html_icon" />
+                        <img className="techIconImage" src={cssIcon} alt="css_icon" />
+                        <img className="techIconImage" src={sassIcon} alt="sass_icon" />
+                        <img className="techIconImage" src={bootstrap} alt="bootstrap_icon" />
+                    {/* </div>
+                    <div> */}
                     <img className="techIconImage" src={gitIcon} alt="git_icon" />
                     <img className="techIconImage" src={nodejsIcon} alt="nodejs_icon" />
                     <img className="techIconImage" src={mySQLIcon} alt="mySQL_icon" />
+                    {/* </div> */}
                 </div>
                 {/* <p className="resume__techs--text">Icons of techs</p> */}
             </section>
