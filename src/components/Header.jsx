a5005ba6021ffefa15a230833dd7d2b719df407c
BiancaMesa <bianca.koraa@gmail.com>
2024-06-11 22:41:08 +0200

    burger menu first draft

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
 import "../scss/components/Header.scss";
 import NavMenu from "./NavMenu";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from "react";
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 
 function Header() {
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header className="header" id="header">
-      {/* <a href="#"> */}
         <h1 className="header__mainName">Bianca Mesa</h1>
-      {/* </a> */}
-        <NavMenu/> 
+        <div className="header__burgerMenu">
+          <FontAwesomeIcon icon={isMobileMenuOpen ? faXmark : faBars} className="header__burgerMenu--icon" onClick={toggleMobileMenu}/>
+          {/* <FontAwesomeIcon icon={faXmark} className="header__burgerMenu--icon" onClick={toggleMobileMenu}/> */}
+        </div>
+        <NavMenu isMobileMenuOpen={isMobileMenuOpen}/> 
     </header>
   )
 }
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,51 +1,74 @@
 import "../scss/components/NavMenu.scss";
 import {Link} from "react-scroll";
 
-function NavMenu() {
+function NavMenu({isMobileMenuOpen}) {
   return (
-    <nav className="menu">
-      <ul className="menu__list">
+    <nav className={`menu ${isMobileMenuOpen ? "menuOpen" : ""}`}>
+      <ul className={`menu__list ${isMobileMenuOpen ? "menuOpen__list" : ""}`}>
 
-      <li className="menu__list--item">
+      <li className={`menu__list--item ${isMobileMenuOpen ? "menuOpen__list--item" : ""}`}>
         <Link to="projects" smooth={true} duration={500} >
           PROJECTS
         </Link>
       </li>
 
-      <li className="menu__list--item dropdown">
+      <li className={`menu__list--item dropdown ${isMobileMenuOpen ? "menuOpen__list--item " : ""}`}>
         <Link to="about" smooth={true} duration={500}>
           ABOUT ME
 
-          <ul className="dropdown__content">
-            <li className="dropdown__content--item">
+          <ul className={`dropdown__content ${isMobileMenuOpen ? "menuOpen__dropdownContent " : ""}`}>
+            <li className="menuOpen__dropdownContent--item ">
               <Link to="skills" smooth={true} duration={500} >
                 SKILLS
               </Link>
             </li>
 
-            <li className="dropdown__content--item">
+            <li className="menuOpen__dropdownContent--item ">
               <Link to="unique" smooth={true} duration={500} >
                 UNIQUE
               </Link>
             </li>
 
-            <li className="dropdown__content--item">      
+            <li className="menuOpen__dropdownContent--item ">     
               <Link to="values" smooth={true} duration={500} >
                 VALUES
               </Link>
             </li>
             
-            <li className="dropdown__content--item">
-            <Link to="passions" smooth={true} duration={500} >
-              PASSIONS
-            </Link>
+            <li className="menuOpen__dropdownContent--item ">
+              <Link to="passions" smooth={true} duration={500} >
+                PASSIONS
+              </Link>
             </li>
           </ul>
         </Link>
       </li>
 
+      <li className={` ${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+        <Link to="skills" smooth={true} duration={500} >
+          SKILLS
+        </Link>
+      </li>
 
-      <li className="menu__list--item">
+      <li className={`${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+        <Link to="unique" smooth={true} duration={500} >
+          UNIQUE
+        </Link>
+      </li>
+
+      <li className={`${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+        <Link to="values" smooth={true} duration={500} >
+          VALUES
+        </Link>
+      </li>
+
+      <li className={`${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+        <Link to="passions" smooth={true} duration={500} >
+          PASSIONS
+        </Link>
+      </li>
+
+      <li className={`menu__list--item ${isMobileMenuOpen ? "menuOpen__list--item" : ""}`}>
         <Link to="contact" smooth={true} duration={500} >
           CONTACT
         </Link>
