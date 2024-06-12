b2d3277a0feca4b413ee18b2cceba92cc187ecd8
BiancaMesa <bianca.koraa@gmail.com>
2024-06-12 19:40:47 +0200

    burger menu open underneath dot added to all items

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -6,17 +6,19 @@ function NavMenu({isMobileMenuOpen}) {
     <nav className={`menu ${isMobileMenuOpen ? "menuOpen" : ""}`}>
       <ul className={`menu__list ${isMobileMenuOpen ? "menuOpen__list" : ""}`}>
 
-      <li className={`menu__list--item ${isMobileMenuOpen ? "menuOpen__list--item" : ""}`}>
+
+      {/* **** UNDERNEATH DOT OPTION 1 ***** */}
+      <li className={`underneathDotItem ${isMobileMenuOpen ? "menuOpen__list--item" : ""}`}>
         <Link to="projects" smooth={true} duration={500} >
           PROJECTS
         </Link>
       </li>
 
-      <li className={`menu__list--item dropdown ${isMobileMenuOpen ? "menuOpen__list--item " : ""}`}>
+      <li className={`underneathDotItem dropdown ${isMobileMenuOpen ? "menuOpen__list--item " : ""}`}>
         <Link to="about" smooth={true} duration={500}>
           ABOUT ME
 
-          <ul className="dropdown__content">
+          <ul className="dropdown__content" >
             <li className="dropdown__content--item">
               <Link to="skills" smooth={true} duration={500} >
                 SKILLS
@@ -44,25 +46,100 @@ function NavMenu({isMobileMenuOpen}) {
         </Link>
       </li>
 
-      <li className={` ${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+      <li className={`underneathDotItem ${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
         <Link to="skills" smooth={true} duration={500} >
           SKILLS
         </Link>
       </li>
 
-      <li className={`${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+      <li className={`underneathDotItem ${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
         <Link to="unique" smooth={true} duration={500} >
           UNIQUE
         </Link>
       </li>
 
-      <li className={`${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+      <li className={`underneathDotItem ${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
         <Link to="values" smooth={true} duration={500} >
           VALUES
         </Link>
       </li>
 
-      <li className={`${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+      <li className={`underneathDotItem ${isMobileMenuOpen ? "menuOpen__list--item" : "hidden"}`}>
+        <Link to="passions" smooth={true} duration={500} >
+          PASSIONS
+        </Link>
+      </li>
+
+      <li className={`underneathDotItem ${isMobileMenuOpen ? "menuOpen__list--item" : ""}`}>
+        <Link to="contact" smooth={true} duration={500} >
+          CONTACT
+        </Link>
+      </li>
+
+
+
+
+
+
+
+      {/* **** UNDERNEATH DOT OPTION 2 ***** */}
+      {/* <li className={`menu__list--item ${isMobileMenuOpen ? "menuOpen__list--item" : ""}`}>
+        <Link to="projects" smooth={true} duration={500} >
+          PROJECTS
+        </Link>
+      </li>
+
+      <li className={`menu__list--item dropdown ${isMobileMenuOpen ? "menuOpen__list--item " : ""}`}>
+        <Link to="about" smooth={true} duration={500}>
+          ABOUT ME
+
+          <ul className="dropdown__content" >
+            <li className="dropdown__content--item">
+              <Link to="skills" smooth={true} duration={500} >
+                SKILLS
+              </Link>
+            </li>
+
+            <li className="dropdown__content--item ">
+              <Link to="unique" smooth={true} duration={500} >
+                UNIQUE
+              </Link>
+            </li>
+
+            <li className="dropdown__content--item ">    
+              <Link to="values" smooth={true} duration={500} >
+                VALUES
+              </Link>
+            </li>
+            
+            <li className="dropdown__content--item ">
+              <Link to="passions" smooth={true} duration={500} >
+                PASSIONS
+              </Link>
+            </li>
+          </ul>
+        </Link>
+      </li>
+
+      <li className={`${isMobileMenuOpen ? "menuOpen__list--item menu__list--item" : "hidden"}`}>
+        <Link to="skills" smooth={true} duration={500} >
+          SKILLS
+        </Link>
+      </li>
+
+      <li className={`${isMobileMenuOpen ? "menuOpen__list--item menu__list--item" : "hidden"}`}>
+        <Link to="unique" smooth={true} duration={500} >
+          UNIQUE
+        </Link>
+      </li>
+
+      <li className={`${isMobileMenuOpen ? "menuOpen__list--item menu__list--item" : "hidden"}`}>
+        <Link to="values" smooth={true} duration={500} >
+          VALUES
+        </Link>
+      </li>
+
+      <li className={`${isMobileMenuOpen ? "menuOpen__list--item menu__list--item" : "hidden"}`}>
         <Link to="passions" smooth={true} duration={500} >
           PASSIONS
         </Link>
@@ -72,7 +149,7 @@ function NavMenu({isMobileMenuOpen}) {
         <Link to="contact" smooth={true} duration={500} >
           CONTACT
         </Link>
-      </li>
+      </li> */}
     </ul>
     </nav>
     
