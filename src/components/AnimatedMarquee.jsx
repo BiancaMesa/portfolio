cccfedf6c46639b01bb855e6c35487c3fd061daa
BiancaMesa <bianca.koraa@gmail.com>
2024-06-12 20:13:54 +0200

    animated marquee moon icon imported from FontAwesome library

--- src/components/AnimatedMarquee.jsx
+++ src/components/AnimatedMarquee.jsx
@@ -1,18 +1,24 @@
 import "../scss/components/AnimatedMarquee.scss";
 import Marquee from "react-fast-marquee";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 function AnimatedMarquee() {
   return (
     // <div className="marqueeContainer">
     <Marquee className="marquee"> 
       <span className="marquee__item">CREATIVE</span> 
-      <i className="fa-solid fa-moon marquee__item"></i>
+      {/* <i className="fa-solid fa-moon marquee__item"></i> */}
+      <FontAwesomeIcon icon={faMoon} className="marquee__item" />
       <span className="marquee__item">ENTHUSIAST</span> 
-      <i className="fa-solid fa-moon marquee__item"></i>
+      {/* <i className="fa-solid fa-moon marquee__item"></i> */}
+      <FontAwesomeIcon icon={faMoon} className="marquee__item" />
       <span className="marquee__item">SUPPORTIVE</span>
-      <i className="fa-solid fa-moon marquee__item"></i>
+      {/* <i className="fa-solid fa-moon marquee__item"></i> */}
+      <FontAwesomeIcon icon={faMoon} className="marquee__item" />
       <span className="marquee__item">SOLUTION-ORIENTED</span>
-      <i className="fa-solid fa-moon marquee__item"></i>
+      {/* <i className="fa-solid fa-moon marquee__item"></i> */}
+      <FontAwesomeIcon icon={faMoon} className="marquee__item" />
     </Marquee>
     // {/* </div> */}
   )
