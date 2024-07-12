8ad82a81f6236b2b18be40d1eef24ef593e7bfe2
BiancaMesa <bianca.koraa@gmail.com>
2024-07-12 18:37:05 +0200

    unique component layout changed

--- src/components/Unique.jsx
+++ src/components/Unique.jsx
@@ -1,7 +1,7 @@
 import "../scss/components/Unique.scss";
 import quoteVideo from "../video/video1.mp4";
 import tech from "../images/tech1.jpg";
-import english from "../images/english1.jpg";
+import english from "../images/english7.png";
 //import coaching from "../images/coaching1.jpg";
 import coaching from "../images/coaching7.png";
 //import yoga from "../images/yoga6.jpg";
@@ -23,34 +23,34 @@ function Unique() {
          <div className="uniqueSection__uniquenesses">
             <div className="uniqueness uniquenessOne">
               <img className="uniqueness__image" src={tech} alt="tech" />
-              <div className="uniqueness__wrapper">
-                <h2 className="uniqueness__wrapper--title">Tech</h2>
-                <p className="uniqueness__wrapper--description">I am in love with technology, coding and design and that makes me pour my heart and love into the projects I undertake.</p>
-              </div>
+              {/* <div className="uniqueness__wrapper"> */}
+                <h2 className="uniqueness__title">Tech</h2>
+                <p className="uniqueness__description">I am in love with technology, coding and design and that makes me pour my heart and love into the projects I undertake.</p>
+              {/* </div> */}
             </div>
 
             <div className="uniqueness uniquenessTwo">
               <img className="uniqueness__image imageTwo" src={english} alt="english" />
-              <div className="uniqueness__wrapper">
-                <h2 className="uniqueness__wrapper--title englishTitle">English</h2>
-                <p className="uniqueness__wrapper--description englishDescription">I have cultivated a fluent level thanks to my big passion for the language. Plus, I thrive off of being in international environments.</p>
-              </div>
+              {/* <div className="uniqueness__wrapper"> */}
+                <h2 className="uniqueness__title englishTitle">English</h2>
+                <p className="uniqueness__description englishDescription">I have cultivated a fluent level thanks to my big passion for the language. Plus, I thrive off of being in international environments.</p>
+              {/* </div> */}
             </div>
 
             <div className="uniqueness uniquenessThree">
               <img className="uniqueness__image" src={coaching} alt="coaching" />
-              <div className="uniqueness__wrapper">
-                <h2 className="uniqueness__wrapper--title">Coaching</h2>
-                <p className="uniqueness__wrapper--description"> The more you understand yourself, the more you will be able to reach your potential. My training as a life coach has revolutionized </p>
-              </div>
+              {/* <div className="uniqueness__wrapper"> */}
+                <h2 className="uniqueness__title">Coaching</h2>
+                <p className="uniqueness__description"> The more you understand yourself, the more you will be able to reach your potential. My training as a life coach has revolutionized </p>
+              {/* </div> */}
             </div>
 
             <div  className="uniqueness uniquenessFour">
               <img className="uniqueness__image imageFour" src={yoga} alt="yoga" />
-              <div className="uniqueness__wrapper">
-                <h2 className="uniqueness__wrapper--title yogaTitle">Yoga</h2>
-                <p className="uniqueness__wrapper--description yogaDescription"> Progress and excellence is an accumulation of small steps in the right direction. This practice has made me reached new levels of patience, consistency, work ethic, and commitment.</p>
-              </div>
+              {/* <div className="uniqueness__wrapper"> */}
+                <h2 className="uniqueness__title">Yoga</h2>
+                <p className="uniqueness__description yogaDescription"> Progress and excellence is an accumulation of small steps in the right direction. This practice has made me reached new levels of patience, consistency, work ethic, and commitment.</p>
+              {/* </div> */}
             </div>
          </div>
 
